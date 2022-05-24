d681fa9517cf2fe7ec946259cefb1de53388ff3b
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-05-23 20:03:41 -0400

    Add details and linting, use Write-Warning instead of Read-Host

--- Get-CompleteMemDump.ps1
+++ Get-CompleteMemDump.ps1
@@ -1,3 +1,24 @@
+<###################################################################################################
+
+.SYNOPSIS
+    Grab a complete memory dump without rebooting.
+.DESCRIPTION
+    Grab a complete memory dump without rebooting. Checks drive space and memory size prior to running. Grabs tools from Windows SDK and Sysinternal automatically. Requires outbound connectivity.
+.INPUTS
+	N/A
+.OUTPUTS
+	None directly, but creates a .dmp file in "C:\Program Files\Common Files" by default.
+.EXAMPLE
+	iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/rjmccallumbigl/PowerShell---Get-Complete-Memory-Dump-Live/main/Get-CompleteMemDump.ps1'))
+.LINK
+	https://github.com/rjmccallumbigl/PowerShell---Get-Complete-Memory-Dump-Live
+.NOTES
+    Author: Ryan McCallum
+    Last Modified: 05-23-2022	
+    v0.1
+	
+####################################################################################################>
+
 # Declare variables
 $baseFolder = "C:\Program Files\Common Files"
 $memory = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum / 1MB
@@ -7,15 +28,16 @@ $dumpLocation = "$($baseFolder)\fullMemory.dmp"
 # Download Windows SDK and install debuggers
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12
 (New-Object System.Net.WebClient).DownloadFile("https://go.microsoft.com/fwlink/?linkid=2173743", "$($baseFolder)\sdksetup.exe")
-if (Test-Path -Path "$($baseFolder)\sdksetup.exe" -PathType Leaf -ErrorAction Stop){
+if (Test-Path -Path "$($baseFolder)\sdksetup.exe" -PathType Leaf -ErrorAction Stop) {
 	& "$($baseFolder)\sdksetup.exe" /features "OptionId.WindowsDesktopDebuggers" /l "$($baseFolder)\windowsSDK.log" /quiet /norestart
-} else {
+}
+else {
 	throw "Could not automatically download SDK, please install manually and retry script: https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/"		
 }
 
 # Download Sysinternals LiveKD for live memory dump
 (New-Object System.Net.WebClient).DownloadFile("https://live.sysinternals.com/livekd.exe", "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe")
-if (Test-Path -Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe" -PathType Leaf -ErrorAction Stop){
+if (Test-Path -Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe" -PathType Leaf -ErrorAction Stop) {
 	
 	# See how much space we need for a complete dump per https://docs.microsoft.com/en-us/troubleshoot/windows-server/performance/memory-dump-file-options#complete-memory-dump
 	Write-Output "This server has about $($memory) MB of memory so you will need at least this + 1MB to save the full dump on the drive at $($dumpLocation)." 
@@ -27,31 +49,29 @@ if (Test-Path -Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd
 		# Seeing if we need to accept EULA before running
 		try {
 			Get-Item "HKCU:\SOFTWARE\Sysinternals\LiveKd" -ErrorAction Stop
-			$eulaResponse = Read-Host "This will attempt to generate a full memory dump without reboot and save it at $($dumpLocation). Type Y or Yes to accept:"
-			# Write-Warning "This will attempt to generate a full memory dump without reboot and save it at $($dumpLocation)." -WarningAction Inquire
+			Write-Warning "This will attempt to generate a full memory dump without reboot and save it at $($dumpLocation)." -WarningAction Inquire
 		}
 		catch {
 			& "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe"
-			$eulaResponse = Read-Host "Do you accept the EULA? Type Y or Yes to accept:"
-			# Write-Warning "Do you accept the EULA? If you do, this script will attempt to generate a full memory dump without reboot and save it at $($dumpLocation)." -WarningAction Inquire
+			Write-Warning "Do you accept the EULA (https://docs.microsoft.com/en-us/sysinternals/license-terms)? If you do, this script will attempt to generate a full memory dump without reboot and save it at $($dumpLocation)." -WarningAction Inquire
 		}
 
-		if (($eulaResponse -ieq "y") -or ($eulaResponse -ieq "yes")) {
-			
-			# Check if we're admin before running, if not run as admin 
-			if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
-				Write-Output "Launching as admin" -
-				Start-Process powershell -Verb runAs -ArgumentList "-noexit & 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe' -accepteula -f -o '$dumpLocation'"
-				Break
-			} else {
-				& 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe' -accepteula -f -o "$($dumpLocation)"
-			}				
-		} else {
-			throw "Not approved, ending script"
-		}		
-	} else {
+		Write-Warning "Press Y on the next prompt to reference the Microsoft symbol server."
+
+		# Check if we're admin before running, if not run as admin 
+		if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+			Write-Output "Launching as admin"
+			Start-Process powershell -Verb runAs -ArgumentList "-noexit & 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe' -accepteula -f -o '$dumpLocation'"
+			Break
+		}
+		else {
+			& 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe' -accepteula -f -o "$($dumpLocation)"
+		}				
+	}
+ else {
 		throw "You do not have enough space to save the dump. You can attach a new disk with available space to the VM and then run the following command via admin PowerShell: ""& 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\livekd.exe' -accepteula -f -ml -o '$($dumpLocation)'"""    			
 	}
-} else {
+}
+else {
 	throw "Could not automatically download LiveKD, please download manually and retry script: https://live.sysinternals.com/livekd.exe"		
 }	
